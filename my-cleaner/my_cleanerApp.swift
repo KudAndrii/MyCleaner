b5f4a9d7f29881c9960620830a93a1e13df882a2
Andrii Kud <cud.andrei@gmail.com>
2026-05-15 17:35:40 +0300

    Initial Commit

---
+++ my-cleaner/my_cleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_cleanerApp.swift
+//  my-cleaner
+//
+//  Created by Andrei Kud on 15.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct my_cleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
